251e73a6b6504064e4983fd1f43d653d94639d7d
Ferry <ferryzxc6@gmail.com>
2019-11-27 12:54:21 +0700

    penambahan java update dan delete

--- app/src/main/java/com/example/penerapansqlite/MyDbAdapter.java
+++ app/src/main/java/com/example/penerapansqlite/MyDbAdapter.java
@@ -2,10 +2,10 @@ package com.example.penerapansqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class MyDbAdapter {
-
     private MyDbHelper myDbHelper;
 
     public MyDbAdapter (Context context){
@@ -20,5 +20,38 @@ public class MyDbAdapter {
         contentValues.put(MyDbHelper.MyPASSWORD,pass);
         long id = dbb.insert(MyDbHelper.TABLE_NAME,null,contentValues);
         return id;
+    }
+    public String getData()
+    {
+        SQLiteDatabase db = myDbHelper.getWritableDatabase();
+        String[] columns = {MyDbHelper.UID,MyDbHelper.NAME,MyDbHelper.MyPASSWORD};
+        Cursor cursor = db.query(myDbHelper.TABLE_NAME,columns,
+                null,null,null,null,null);
+        StringBuffer buffer = new StringBuffer();
+        while (cursor.moveToNext())
+        {
+            int cid = cursor.getInt(cursor.getInt(cursor.getColumnIndex(myDbHelper.UID));
+            String name = cursor.getInt(cursor.getString(cursor.getColumnIndex(myDbHelper.NAME));
+            String password = cursor.getInt(cursor.getString(cursor.getColumnIndex(myDbHelper.MyPASSWORD));
+            buffer.append(cid + " " + name + " " + password + "\n");
+        }
+        return buffer.toString();
+    }
+    public int delete(String uname)
+    {
+        SQLiteDatabase db = myDbHelper.getWritableDatabase();
+        String[] whereArgs = {uname};
+
+        int count = db.delete(MyDbHelper.TABLE_NAME , MyDbHelper.NAME + "= ?",whereArgs);
+        return count;
+    }
+    public int updateName(String oldName , String newName)
+    {
+        SQLiteDatabase db = myDbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MyDbHelper.NAME,newName);
+        String[] whereArgs = {oldName};
+        int count = db.update(MyDbHelper.TABLE_NAME,contentValues,myDbHelper.NAME+" = ?",whereArgs);
+        return count;
     }
 }
